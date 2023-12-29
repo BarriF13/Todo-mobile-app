ac23f93a759424e6b3c2032effc60d4c1dbc63c6
BarriF <barrifarjadnia@hollandandbarrett.com>
2023-12-29 11:09:32 +0000

    add flex style

--- RNMAXproject/App.js
+++ RNMAXproject/App.js
@@ -7,9 +7,9 @@ export default function App() {
     <View  style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput placeholder="Your course goal" style={styles.textInput} />
-        <Button title="Add goal" />
+        <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals</Text>
       </View>
     </View>
@@ -19,18 +19,29 @@ export default function App() {
 const styles = StyleSheet.create({
 
   appContainer:{
-   
-    padding: 50
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer:{
+    flex:1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    
   },
   textInput:{
     borderWidth: '1px',
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
-    padding: 8
+    padding: 8,
+    borderRadius: 10,
+  },
+  goalsContainer: {
+    flex: 4,
   }
 });
