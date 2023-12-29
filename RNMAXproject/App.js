9d16ef84d441bec3e578451c7f14d14068224beb
BarriF <barrifarjadnia@hollandandbarrett.com>
2023-12-29 10:50:09 +0000

    feat: set up a new mobile app project

---
+++ RNMAXproject/App.js
@@ -0,0 +1,36 @@
+
+import { StyleSheet, Text, View,Button, TextInput} from 'react-native';
+
+
+export default function App() {
+  return (
+    <View  style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course goal" style={styles.textInput} />
+        <Button title="Add goal" />
+      </View>
+      <View>
+        <Text>List of goals</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  appContainer:{
+   
+    padding: 50
+  },
+  inputContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput:{
+    borderWidth: '1px',
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
+  }
+});
